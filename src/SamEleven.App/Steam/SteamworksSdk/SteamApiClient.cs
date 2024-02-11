1fc8aa6d37eacdae56428bc280ba6ab669a9b0a5
emyfreya <emyfreya+github@gmail.com>
2024-02-11 19:57:31 +0100

    csproj configuration

--- src/SamEleven.App/Steam/SteamworksSdk/SteamApiClient.cs
+++ src/SamEleven.App/Steam/SteamworksSdk/SteamApiClient.cs
@@ -5,7 +5,7 @@ internal sealed class SteamApiClient
     public uint[] GetAppList()
     {
         Emyfreya.Steam.Abstractions.ISteamService steamClient = Emyfreya.Steam.SteamServiceFactory.Build();
-        string name = steamClient.GetAppName(420).ValueOrDefault;
+        string name = steamClient.GetAppName(2626940).ValueOrDefault;
 
         return [];
     }
